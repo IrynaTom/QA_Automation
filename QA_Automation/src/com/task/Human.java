3669d449c23b838307e02738ef8f213b64903760
IrynaTom <tomanova.iryna@gmail.com>
2016-02-17 22:50:18 +0200

    Third
    - add choose between "Search" and "Add"
    - add field "E-mail"
    - add users to collection
    - add verification(not works)

--- QA_Automation/src/com/task/Human.java
+++ QA_Automation/src/com/task/Human.java
@@ -6,12 +6,14 @@ package com.task;
 public class Human {
     private String name;
     private int age;
+    private String email;
 
-    public Human(String name, int age){
+    public Human(String name, int age,String email){
         this.name = name;
         this.age=age;
+        this.email=email;
     }
     public void printArray(){
-        System.out.println("Name" + " " + this.name + " " + "Age" + " " + this.age);
+        System.out.println("Name" + " " + this.name + " " + "Age" + " " + this.age + " " +"E-mail" + " "+this.email );
     }
 }
--- QA_Automation/src/com/task/Second.java
+++ QA_Automation/src/com/task/Second.java
@@ -1,4 +1,8 @@
 package com.task;
+import com.sun.istack.internal.Nullable;
+import sun.invoke.empty.Empty;
+
+import java.util.ArrayList;
 import java.util.Scanner;
 
 
@@ -7,31 +11,46 @@ import java.util.Scanner;
  */
 public class Second {
     public static void main (String[] args) {
-        int count=0;
-        Human[] array= new Human[5];
-        while (array.length > count) {
-            System.out.println("Input your name, please: ");
-            Scanner sc = new Scanner(System.in); // создаём объект класса Scanner
-            String name = sc.nextLine();
-            System.out.println("Input your age, please: ");
-            String sn = sc.nextLine();
+        Scanner sc = new Scanner(System.in);// создаём объект класса Scanner
+        ArrayList<Human> list = new ArrayList<Human>();// создаем список
+        // Делается выбор юзера
+        System.out.println("1. Add new person\n2. Search people");
+        System.out.println("Please input your choose:");
+        String f = sc.nextLine();
+        int c = Integer.parseInt(f);//преобразование сроки в число типа int
+        // Добавление юзеров
+        if (c == 1) {
+            while (list.size() <= 5) {
+                System.out.println("Input your name, please: ");
+                String name = sc.nextLine();
+                System.out.println("Input your age, please: ");
+                String sn = sc.nextLine();
+                int age = Integer.parseInt(sn);//преобразование сроки в число типа int
+                System.out.println("Input your E-mail, please: ");
+                String email = sc.nextLine();
+//проврека на пустые поля
+                if (name == null || name.length() == 0 || sn==null ||sn.length()==0) {
+                    System.out.println("The name is required field");
+                }
+                else  if (age > 100 || age < 0 || name.length() < 4 ) {
+                    System.out.println("Wrong data, try again");
+                    continue;
+                }
 
-            int age = Integer.parseInt(sn);//преобразование сроки в число типа int
 
 
-            if (age > 100 || age < 0 ||name.length()<4) {
-                System.out.println("Wrong data, try again");
-                continue;
+                list.add(new Human(name, age, email));
+
             }
-
-            array[count] = new Human(name, age);
-            count++;
         }
-        System.out.println("Array is over. Result array:");
-        for(Human human: array){
-            human.printArray();
+        // поиск юзеров
+            else {
+                System.out.println("Search");
+            }
+            // System.out.println("Result array:");
+            //for(Human human: list){
+            //  human.printArray();
         }
     }
 
-}
 
